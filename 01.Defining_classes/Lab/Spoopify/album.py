cfefbde3510eca7713124cb1366454cc4ac8b4b0
Emil Darakchiev <72097603+edarakchiev@users.noreply.github.com>
2021-03-02 18:04:07 +0200

    Add files via upload

---
+++ 01.Defining_classes/Lab/Spoopify/album.py
@@ -0,0 +1,40 @@
+# from song import Song
+
+
+class Album:
+    def __init__(self, name, *args):
+        self.name = name
+        self.songs = [song for song in args]
+        self.published = False
+
+    def add_song(self, song):
+        if self.published:
+            return "Cannot add songs. Album is published."
+        if song.single:
+            return f"Cannot add {song.name}. It's a single"
+        if song not in self.songs:
+            self.songs.append(song)
+            return f"Song {song.name} has been added to the album {self.name}."
+        return "Song is already in the album."
+
+    def remove_song(self, song_name):
+        if self.published:
+            return "Cannot remove songs. Album is published."
+        for song in self.songs:
+            if song_name == song.name:
+                self.songs.remove(song)
+                return f"Removed song {song.name} from album {self.name}."
+        return "Song is not in the album."
+
+    def publish(self):
+        if self.published:
+            return f"Album {self.name} is already published."
+        self.published = True
+        return f"Album {self.name} has been published."
+
+    def details(self):
+        result = f"Album {self.name}\n"
+        for s in self.songs:
+            result += f"== {s.get_info()}\n"
+        return result
+

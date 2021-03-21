dd4118f6e0eed369e94a6c4f563ddef5e4b25917
Emil Darakchiev <72097603+edarakchiev@users.noreply.github.com>
2021-03-21 10:47:55 +0200

    Add files via upload

---
+++ 06.polymorphism/exercise/wild_farm/animals/mammals.py
@@ -0,0 +1,58 @@
+from wild_farm.animals.animal import Mammal
+from wild_farm.food import Vegetable, Fruit, Meat
+
+
+class Mouse(Mammal):
+    def __init__(self, name, weight, living_region):
+        super().__init__(name, weight, living_region)
+
+    def make_sound(self):
+        return "Squeak"
+
+    def feed(self, food):
+        if not isinstance(food, Vegetable) and not isinstance(food, Fruit):
+            return f"{self.__class__.__name__} does not eat {food.__class__.__name__}!"
+        self.food_eaten += food.quantity
+        self.weight += 0.1 * food.quantity
+
+
+class Dog(Mammal):
+    def __init__(self, name, weight, living_region):
+        super().__init__(name, weight, living_region)
+
+    def make_sound(self):
+        return "Woof!"
+
+    def feed(self, food):
+        if not isinstance(food, Meat):
+            return f"{self.__class__.__name__} does not eat {food.__class__.__name__}!"
+        self.food_eaten += food.quantity
+        self.weight += 0.4 * food.quantity
+
+
+class Cat(Mammal):
+    def __init__(self, name, weight, living_region):
+        super().__init__(name, weight, living_region)
+
+    def make_sound(self):
+        return "Meow"
+
+    def feed(self, food):
+        if not isinstance(food, Vegetable) and not isinstance(food, Meat):
+            return f"{self.__class__.__name__} does not eat {food.__class__.__name__}!"
+        self.food_eaten += food.quantity
+        self.weight += 0.3 * food.quantity
+
+
+class Tiger(Mammal):
+    def __init__(self, name, weight, living_region):
+        super().__init__(name, weight, living_region)
+
+    def make_sound(self):
+        return "ROAR!!!"
+
+    def feed(self, food):
+        if not isinstance(food, Meat):
+            return f"{self.__class__.__name__} does not eat {food.__class__.__name__}!"
+        self.food_eaten += food.quantity
+        self.weight += 1.0 * food.quantity

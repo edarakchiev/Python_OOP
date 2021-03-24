2443dd991153d0cc76e356d3ee499d3d95bd5c95
Emil Darakchiev <72097603+edarakchiev@users.noreply.github.com>
2021-03-24 16:20:46 +0200

    Delete 09.possible_permutation.py

--- 07.iterators_and_generators/09.possible_permutation.py
+++
@@ -1,9 +0,0 @@
-from itertools import permutations
-
-
-def possible_permutations(data):
-    for per in permutations(data):
-        yield list(per)
-
-
-[print(n) for n in possible_permutations([1, 2, 3])]
